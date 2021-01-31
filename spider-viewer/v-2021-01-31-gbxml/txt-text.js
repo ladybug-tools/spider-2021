85d68b4662355a58ad0436c32123a211b7319548
Theo Armour <t.armour@gmail.com>
2021-01-31 15:17:47 -0800

    speech fix

--- spider-viewer/v-2021-01-31-gbxml/txt-text.js
+++ spider-viewer/v-2021-01-31-gbxml/txt-text.js
@@ -19,46 +19,95 @@ TXT.init = function () {
 
 	loader.load( url, ( fnt ) => font = fnt );
 
-	THR.ground.position.z = -500
+	THR.ground.position.z = -500;
 
 	htm = `
 <details open>
-<summary class="summary-primary gmd-1" >gbXML BoD Presentation</summary>
-		<p>
-		<button onclick=TXT.addText1() >Title</button>
-	</p>
-
+	<summary class="summary-primary gmd-1" >gbXML BoD Presentation</summary>
 	<p>
-		<button onclick=TXT.addText2() >Second Post</button>
+		<button onclick=TXT.addText0() >Title</button>
 	</p>
-
 	<p>
-		<button onclick=TXT.addText3()>Third Post</button>
+		<button onclick=TXT.addText1()>Mission 1: fewer viewers</button>
+	</p>
+	<p>
+		<button onclick=TXT.addText2() >Mission 2: prettier models</button>
+	</p>
+	<p>
+		<button onclick=TXT.addText3() >Mission 3: "Don't make me change!"</button>
+	</p>
+	<p>
+		<button onclick=TXT.addText4() >As always: build better buildings</button>
+	</p>
+	<p>
+		<button onclick=TXT.addText5() >Call to action: Where's the action?</button>
 	</p>
 </details>`;
 
-	TXTdivDetails.innerHTML = htm
+	TXTdivDetails.innerHTML = htm;
 
 };
 
 
-TXT.addText1 = function() {
+TXT.addText0 = function () {
 
-	text = "gbXML\nBoard of Directors\n2021-01-31";
+	TXT.setNewGroup();
+
+	THR.ground.position.z = -50;
+
+	THR.camera.position.set( -30, -80, 0 );
+
+	text = "gbXML\nBoard of Directors Meeting\n2021-01-31";
 	color = 0x11aa33;
 	size = 8;
 	x = -5;
-	y = 38;
+	y = 35;
 	z = -30; //- THR.center.z;
 	console.log( "text", text );
 	TXT.group.add( TXT.getSimpleText( { text, color, size, x, y, z } ) );
 
 	//TXT.group.add( TXT.getSimpleText( { text, color, size, x, y, z } ) );
 
-	TXT.group.add( TXT.getSimpleText( { text: "www.ladybug.tools/spider-2021/spider-viewer/", color: 0x0000ff, size: 3, x: -15, y: 20, z: 0 } ) );
+	text2 = `
+www.ladybug.tools/spider-2021/spider-viewer/
+Theo Armour
+t.armour@gmail.com`
+
+	TXT.group.add( TXT.getSimpleText( { text: text2, color: 0x0000ff, size: 5, x: -15, y: 0, z: 0 } ) );
 
 
-}
+};
+
+
+
+TXT.addText1 = function () {
+
+	TXT.setNewGroup();
+
+	THR.ground.position.z = -50;
+
+	THR.camera.position.set( -30, -80, 0 );
+
+	//location.hash = "https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/omha-nb-zneth.xml"
+
+	const text = `
+Mission 1
+Problem: too many viewers
+Solution : single viewer opens
+>> gbXML, Radiance, IDF & OSM,
+>> Honeybee JSON and more`;
+
+	const color = 0xaa6644;
+	const size = 8;
+	const x = -5;
+	const y = 45;
+	const z = -30; //- THR.center.z;
+	//console.log( "text", text );
+
+	TXT.group.add( TXT.getSimpleText( { text, color, size, x, y, z } ) );
+
+};
+
 
 TXT.addText2 = function () {
 
@@ -66,12 +115,12 @@ TXT.addText2 = function () {
 
 	THR.ground.position.z = -50;
 
-	THR.camera.position.set( -30, -80, 0 )
+	THR.camera.position.set( -30, -80, 0 );
 
 	//location.hash = "https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/omha-nb-zneth.xml"
 
 	const text = `
-Mission 1
+Mission 2
 Problem: "Make the models pretty"
 Solution : export files to prettifiers
 Now support translate and export to
@@ -93,7 +142,92 @@ TXT.addText3 = function () {
 
 	TXT.setNewGroup();
 
-	TXT.group.add( TXT.getSimpleText( { text: "Third post", color: 0xff00ff, x: -20, y: 20 } ) );
+	THR.ground.position.z = -50;
+
+	THR.camera.position.set( -30, -80, 0 );
+
+	//location.hash = "https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/omha-nb-zneth.xml"
+
+	const text = `
+Mission 3
+* Send gbXML files to OpenStreetMaps
+* Send gbXML to Maya, Rhino and Blender
+* Fix errors & pass ASHRAE Validator
+`;
+
+	const color = 0x0000dd;
+	const size = 8;
+	const x = -5;
+	const y = 45;
+	const z = -30; //- THR.center.z;
+	//console.log( "text", text );
+
+	TXT.group.add( TXT.getSimpleText( { text, color, size, x, y, z } ) );
+
+};
+
+
+
+TXT.addText4 = function () {
+
+	TXT.setNewGroup();
+
+	THR.ground.position.z = -50;
+
+	THR.camera.position.set( -30, -80, 0 );
+
+	//location.hash = "https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/omha-nb-zneth.xml"
+
+	const text = `
+As always
+* Fast on phone, tablet and workstation
+* Choose your version - long term support
+* Plain vanilla easy-peasy JavaScript
+--Pollination:React|PerkinsWill:Angular, BuildSim??
+* FOSS on GitHub/no charge consulting
+`;
+
+	const color = 0x0044dd;
+	const size = 8;
+	const x = -5;
+	const y = 45;
+	const z = -30; //- THR.center.z;
+	//console.log( "text", text );
+
+	TXT.group.add( TXT.getSimpleText( { text, color, size, x, y, z } ) );
+
+};
+
+
+
+
+TXT.addText5 = function () {
+
+	TXT.setNewGroup();
+
+	THR.ground.position.z = -50;
+
+	THR.camera.position.set( -30, -80, 0 );
+
+	//location.hash = "https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/omha-nb-zneth.xml"
+
+	const text = `
+Call to action / Looking for team
+* Community manager
+* Marketing help
+* Co-developers
+Have 100s users average 8 minutes per session
+Start-up opportunity for peeps younger than me
+`;
+
+	const color = 0x0044dd;
+	const size = 8;
+	const x = -5;
+	const y = 45;
+	const z = -30; //- THR.center.z;
+	//console.log( "text", text );
+
+	TXT.group.add( TXT.getSimpleText( { text, color, size, x, y, z } ) );
 
 };
 
@@ -102,7 +236,7 @@ TXT.getSimpleText = function ( {
 	color = 0x006699,
 	size = 10,
 	x = 0,
-	y= 0,
+	y = 0,
 	z = 0
 } = {} ) {
 
@@ -117,7 +251,7 @@ TXT.getSimpleText = function ( {
 	const material = new THREE.MeshBasicMaterial( { color: color, opacity: 0.8, side: 2, transparent: true } );
 
 	const mesh = new THREE.Mesh( geometry, material );
-	mesh.castShadow = true
+	mesh.castShadow = true;
 
 	return mesh;
 
@@ -127,33 +261,12 @@ TXT.getSimpleText = function ( {
 
 TXT.setNewGroup = function () {
 
-
 	THR.getGroupNew();
-
-	//THR.scene.remove( TXT.group );
-
-	TXT.group = new THREE.Group();
-
-	THR.scene.add( TXT.group );
-
-};
-
-
-TXT.addTextContinents = function () {
 
 	THR.scene.remove( TXT.group );
 
 	TXT.group = new THREE.Group();
 
-	//TXT.addBox();
-
-	TXT.getSimpleText( { text: "Africa\n123", color: 0x0000, radius: 65, latitude: "0", longitude: "0" } );
-	TXT.getSimpleText( { text: "Europe", color: 0x0085C7, radius: 65, latitude: "50", longitude: "50" } );
-	TXT.getSimpleText( { text: "Asia", color: 0xF4C300, radius: 60, latitude: "20", longitude: "130" } );
-	TXT.getSimpleText( { text: "Oceania", color: 0x009F3D, latitude: "-10", longitude: "160" } );
-	TXT.getSimpleText( { text: "Americas", color: 0xDF0024, radius: "70", latitude: "0", longitude: "-100" } );
-
 	THR.scene.add( TXT.group );
 
 };
-
