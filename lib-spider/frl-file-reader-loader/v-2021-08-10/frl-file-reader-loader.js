90f2129a27862a6ada47ba507c3cbeab1526de31
Theo Armour <t.armour@gmail.com>
2021-08-10 18:52:08 -0700

    update readme

---
+++ lib-spider/frl-file-reader-loader/v-2021-08-10/frl-file-reader-loader.js
@@ -0,0 +1,217 @@
+// copyright 2021 Theo Armour. MIT license.
+/* global */
+// jshint esversion: 6
+// jshint loopfunc: true
+
+
+const FRL = {};
+
+//FRL.r3dmParser = undefined;
+FRL.gltfParser = undefined;
+FRL.idfParser = undefined;
+FRL.objParser = undefined;
+FRL.stlParser = undefined;
+FRL.vtkParser = undefined;
+
+FRL.init = function () {
+
+
+	FRLdivMenuFileRead.innerHTML = `
+<details id=detFile>
+
+	<summary class="summary-primary gmd-1" title="View selected items">
+		File menu
+
+		<span class="info">
+			<img class=infoImg src="../../lib/assets/icons/noun_Information_585560.svg">
+			<div class="infoTooltip gmd-5">
+				<div>
+					Open <a href="http://gbxml.org" target="_blank">gbXML</a>,
+					gLTF, IDF & OSM, OBJ, STL, VTK Files
+				</div>
+				<!-- <p><a href="https://www.energyplus.net/" target="_blank">EnergyPlus</a> IDF and OSM files</p> -->
+			</div>
+		</span>
+
+	</summary>
+
+	<div id=FRdivMenuFileReader> </div>
+
+	<p>
+		<input type=file id=FRLinpFile onchange=FRL.readFile(this); accept="*">
+	</p>
+
+	<div id=FRLdivLog></div>
+
+	<div id=FOZdivFileOpenZip></div>
+
+</details>`;
+
+};
+
+
+
+FRL.readFile = function ( files ) {
+
+	FRL.timeStart = performance.now();
+	FRL.files = files;
+	FRL.file = FRL.files.files[ 0 ];
+
+	FRL.reader = new FileReader();
+	FRL.reader.onload = FRL.onLoad;
+
+	FRL.reader.readAsText( FRL.file );
+
+};
+
+
+FRL.onLoad = function () {
+
+	console.log( "file", FRL.file );
+
+	FRL.fileName = FRL.file.name;
+	FRL.hostName = FRL.file.type;
+
+	string = FRL.reader.result;
+	//console.log( "", string );
+
+	const fname = FRL.file.name.toLowerCase();
+
+	if ( fname.endsWith( ".zip" ) ) {
+
+		// FRL.dataZip = FRL.file;
+		// //console.log( "name", FRL.file.name );
+		// FOZ.onLoadFile();
+
+	}
+
+	if ( fname.endsWith( "xml" ) ) {
+
+		GBX.parseResponse( string );
+
+		FRL.onProgress( FRL.file.size, "Load complete" );
+
+		return;
+
+	}
+
+	if ( fname.endsWith( "gltf" ) || fname.endsWith( "glb" ) ) {
+
+		if ( FRL.gltfParser === undefined ) {
+
+			FRL.gltfParser = document.body.appendChild( document.createElement( 'script' ) );
+			FRL.gltfParser.onload = () => GLTF.readGltf( FRL.files );
+			FRL.gltfParser.src = "https://www.ladybug.tools/spider-2021/lib-spider/gltf-parser/v-2021-01-29/gltf-parser.js";
+
+		} else {
+
+			GLTF.readGltf( FRL.files );
+
+		}
+
+		return;
+
+	}
+
+	if ( fname.endsWith( "idf" ) || fname.endsWith( "osm" ) ) {
+
+		if ( FRL.idfParser === undefined ) {
+
+			FRL.idfParser = document.body.appendChild( document.createElement( 'script' ) );
+			FRL.idfParser.onload = () => IDF.parseString( string );
+			FRL.idfParser.src = "https://www.ladybug.tools/spider-2021/lib-spider/idf-parser/v-2021-01-27/idf-parser.js";
+
+		} else {
+
+			IDF.parseString( string );
+
+		}
+
+		FRL.onProgress( FRL.file.size, "Load complete" );
+
+		return;
+
+	}
+
+	if ( fname.endsWith( ".obj" ) ) {
+
+		if ( FRL.objParser === undefined ) {
+
+			FRL.objParser = document.body.appendChild( document.createElement( 'script' ) );
+			FRL.objParser.onload = () => OBJ.loadOBJ( FRL.files );
+			FRL.objParser.src = "https://www.ladybug.tools/spider-2021/lib-spider/obj-parser/v-2021-01-30/obj-parser.js";
+
+		} else {
+
+			OBJ.loadObj( FRL.files );
+
+		}
+
+		FRL.onProgress( FRL.file.size, "Load complete" );
+
+		return;
+
+	}
+
+	if ( fname.endsWith( ".stl" ) ) {
+
+		if ( FRL.stlParser === undefined ) {
+
+			FRL.stlParser = document.body.appendChild( document.createElement( 'script' ) );
+			FRL.stlParser.onload = () => STL.loadSTL( FRL.files );
+			FRL.stlParser.src = "https://www.ladybug.tools/spider-2021/lib-spider/stl-parser/v-2021-01-30/stl-parser.js";
+
+		} else {
+
+			STL.loadSTL( FRL.files );
+
+		}
+
+		FRL.onProgress( FRL.file.size, "Load complete" );
+
+		return;
+
+	}
+
+	if ( fname.endsWith( "vtk" ) || fname.endsWith( "vtp" ) ) {
+
+		if ( FRL.vtkParser === undefined ) {
+
+			FRL.vtkParser = document.body.appendChild( document.createElement( 'script' ) );
+			FRL.vtkParser.onload = () => VTK.readVTK( FRL.files  );
+			FRL.vtkParser.src = "https://www.ladybug.tools/spider-2021/lib-spider/vtk-parser/v-2021-01-27/vtk-parser.js";
+
+		} else {
+
+			VTK.readVTK( FRL.files );
+
+		}
+
+		return;
+
+	}
+};
+
+
+FRL.onProgress = function ( size = 0, note = "" ) {
+
+	FRL.timeToLoad = ( performance.now() - FRL.timeStart ).toLocaleString();
+	FRL.size = size;
+
+	// const a = document.createElement( 'a' );
+	// a.href = url;
+	// FOO.hostName = a.hostname;
+	const htm =
+		`
+		<p>
+			<span class=attributeTitle >File name</span>: <span class=attributeValue >${ FRL.fileName }</span></br>
+			<span class=attributeTitle >Host|type</span>: <span class=attributeValue >${ FRL.hostName }</span></br>
+ 			<span class=attributeTitle >Bytes loaded</span>: <span class=attributeValue >${ size.toLocaleString() }</span></br>
+			<span class=attributeTitle >Time to load</span>: <span class=attributeValue>${ FRL.timeToLoad } ms</span></br>
+			${ note }
+		</p>
+	`;
+
+	FRLdivLog.innerHTML = htm;
+
+	};
---
+++ spider-viewer/v-2021-08-10/README.md
@@ -0,0 +1,58 @@
+# gbXML BoD 2021-02-01 Read Me
+
+## Title
+
+
+## State of the Art
+
+Problem: many AEC formats
+gbXML plus IFC, IDF & OSM, Radiance, Honeybee JSON and more
+Adding terrain and complex shade elements
+Solution: gbXML plays well with all the formats
+Comparison: gbXML stores much design intent in simple format
+
+
+
+## Mission 1 ~ Not used
+
+Mission 1
+Problem: too many viewers
+Solution : single viewer opens
+>> gbXML, Radiance, IDF & OSM,
+>> Honeybee JSON and more`;
+
+## Mission
+
+"Make the models pretty" was difficult to solve
+Solution 1: export files to prettifiers
+Now support translate and export to
+* STL, OBJ, glTF, JSON and gbXML?!?
+
+
+
+## Mission 3
+
+* Import gbXML files into OpenStreetMaps
+* Import gbXML into Maya, Rhino and Blender
+* Fix errors to pass the ASHRAE Validator
+
+## As always
+
+* Choose your version - with long term support
+* Easy-peasy code:
+	* Pollination using React, Perkins Will using Angular, BuildSim??
+* Works on phone, tablet and workstation
+* FOSS in GitHub
+
+## Call to action
+
+Looking for
+
+* Community manager
+* Marketing help
+* Co-developers
+
+Hundreds of users average 8 minutes per session
+
+Start-up opportunity for someone younger than me
+
