a49f3dbe4cab221d7e635c01b1e428f785c439ff
Theo Armour <t.armour@gmail.com>
2021-08-17 13:32:17 -0700

    update readme

---
+++ spider-viewer/v-2021-08-17/spider-viewer.js
@@ -0,0 +1,124 @@
+
+
+
+const COR = {
+	user: "ladybug-tools",
+	repo: "spider-2021",
+	branch: "main",
+	path: "../../",
+	defaultFile: "README.md",
+	ignoreFolders: [],
+	//ignoreFolders: ["archive", "lib", "lib3d", "lib-templates"],
+	filterFiles: [ "gif", "md", "jpg", "html", "license", "pdf", "png", "svg", "txt" ],
+	urlSource: "https://github.com/ladybug-tools/spider-2021/tree/main/spider-viewer",
+	urlAssets: "https://pushme-pullyou.github.io/tootoo-2021/",
+	iconGitHub: `<img src="https://pushme-pullyou.github.io/tootoo-2021/lib/assets/icons/mark-github.svg">`,
+	iconInfo: `<img class=infoImg src="https://pushme-pullyou.github.io/tootoo-2021/lib/assets/icons/noun_Information_585560.svg">`,
+	iconExternalFile: `<img class=infoImg  src="https://pushme-pullyou.github.io/tootoo-2021/lib/assets/icons/icon-external-link.svg">`,
+	title: document.title ? document.title : location.href.split( "/" ).pop().slice( 0, - 5 ).replace( /-/g, " " ),
+	version: document.head.querySelector( "[ name=date ]" ).content,
+	description: document.head.querySelector( "[ name=description ]" ).content,
+};
+
+const description = `
+Online interactive <a href="https://www.gbxml.org" target="_blank">gbXML</a> in 3D viewer in your browser
+designed to be forked, hacked and remixed using the WebGL and the
+<a href="https://threejs.org" target="_blank">Three.js</a> JavaScript library`;
+
+
+COR.files = [
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/annapolis-md-single-family-residential-2016.xml",
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/3d-models@master/gbxml-sample-files/files-open-fast/8-a.xml",
+	"https://cdn.jsdelivr.net/gh/GreenBuildingXML/Sample-gbXML-Files@master/ConferenceCenter%20(Older).xml",
+	"https://cdn.jsdelivr.net/gh/GreenBuildingXML/Sample-gbXML-Files@master/gbXML_TRK.xml",
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/spider@master/gbxml-sample-files/zip/bilt-2019-template.zip",
+	"https://www.ladybug.tools/spider/gbxml-sample-files/bristol-clifton-downs-fixed.xml",
+	"https://www.ladybug.tools/spider/gbxml-sample-files/bristol-clifton-down-road-utf16.xml",
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/spider@master/gbxml-sample-files/zip/warwick-university-5k-surfaces.zip",
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/spider@master/gbxml-sample-files/annapolis-md-single-family-residential-2016.xml",
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/spider@master/gbxml-sample-files/zip/pittsburg-airport.zip",
+	"https://www.ladybug.tools/spider/gbxml-sample-files/aspen-co-resort-retail.xml",
+	"https://www.ladybug.tools/spider/gbxml-sample-files/samples-2/Berlin_Office_SAM2017.xml",
+	"https://GreenBuildingXML.github.io/Sample_gbXML_Files/ChapelHillOffice.xml",
+	"https://cdn.jsdelivr.net/gh/ladybug-tools/spider@master/gbxml-sample-files/samples-2/18141-M18.xml",
+	"https://www.ladybug.tools/3d-models/gbxml-sample-files/revit-sample-files/2020_rac_advanced_sample_project.xml",
+	"https://ladybug.tools/3d-models/idf-sample-files/2020/HospitalLowEnergy.idf",
+	"../../lib-spider/idf-parser/HospitalLowEnergy.idf",
+	"../../lib-spider/gltf-parser/Duck1.gltf"
+
+
+
+];
+
+function init () {
+
+	//GOR.path = GRV.path = \
+
+	MNU.path = "../../";
+	MNU.init();
+
+	sumNavMenu.hidden = false;
+
+	FRL.init();
+
+	GFF.init();
+	//GFF.source = "../../lib-spider/gff-github-folder-files/gff-multiple.js";
+
+	EXP.init();
+
+	THR.init();
+
+	THR.animate();
+
+	THR.addLights();
+
+	THR.addGround();
+
+	THR.group = THR.getGroupNew();
+
+	//THRU.addMeshes( 100 );
+	//THR.zoomObjectBoundingSphere();
+
+	FOX.init();
+	FOX.defaultFile = COR.files[ 14 ];
+	FOX.onHashChange();
+
+	TXT.init();
+
+	// if running on server, keep address bar pointed to latest rev
+
+	if ( !location.hash && location.protocol === "https:" ) {
+
+		window.history.pushState( "", "", "../" );
+
+		COR.path = "https://www.ladybug.tools/spider-2021/";
+
+	} else {
+
+		COR.path = "../../";
+
+		THR.controls.autoRotate = false;
+
+	}
+
+};
+
+
+COR.reset = function ( meshes = [] ) {
+
+	if ( chkNewFile.checked ) { THR.group = THR.getGroupNew(); }
+
+	const child = new THREE.Group();
+	child.add( ...meshes );
+
+	THR.group.add( child );
+
+	THR.zoomObjectBoundingSphere();
+	//THRU.toggleBoundingBoxHelper();
+
+	dragControls = new THREE.DragControls( [ child ], THR.camera, THR.renderer.domElement );
+	dragControls.transformGroup = true;
+	dragControls.addEventListener( 'dragstart', function ( event ) { THR.controls.enabled = false; } );
+	dragControls.addEventListener( 'dragend', function ( event ) { THR.controls.enabled = true; } );
+
+};
